8e6eb325982ef7496ec0a21a1c9b6c5ea305716e
Stephen Gould <stephen.gould@anu.edu.au>
2014-07-25 18:39:32 +1000

    bug fix in rosetta script

--- projects/rosetta/getRosettaData.py
+++ projects/rosetta/getRosettaData.py
@@ -14,15 +14,24 @@ fname = "Rosetta_Design_Dataset.tgz"
 if (os.path.isfile(fname) == 0):
     print("WARNING: this make take a while...")
     u = "http://jmlr.csail.mit.edu/papers/volume7/yanover06a/Rosetta_Design_Dataset.tgz"
-    urllib.request.urlretrieve(u, fname)
+    try:
+        urllib.request.urlretrieve(u, fname)
+    except:
+        print("ERROR: could not download data file " + fname)
+        exit()
 
 # untar files
+print("decompressing data files...")
 tarfile.open(fname, "r:gz").extractall(".")
 
 # fetch sparse class
 fname = "sparse_cell_2.tgz"
 if (os.path.isfile(fname) == 0):
-    print "fetching sparse cell class..."
+    print("fetching sparse cell class...")
     u = "http://cyanover.fhcrc.org/sparse_cell_2.tgz"
-    urllib.request.urlretrieve(u, fname)
-    tarfile.open(fname, "r:gz").extractall(".")
+    try:
+        urllib.request.urlretrieve(u, fname)
+        tarfile.open(fname, "r:gz").extractall(".")
+    except:
+        print("ERROR: could not download data file " + fname)
+        exit()
