f5d580fec9c31c50b35b1115cba3cf062d55cbad
Stephen Gould <sgould@lexington.(none)>
2014-07-31 12:42:28 +1000

    changed generateSuperpixel application to support multiple parameter settings

--- src/app/generateSuperpixels.cpp
+++ src/app/generateSuperpixels.cpp
@@ -35,8 +35,8 @@ void usage()
     cerr << "USAGE: ./generateSuperpixels [OPTIONS] <img>\n";
     cerr << "OPTIONS:\n"
          << "  -m <method>       :: segmentation method (SUPERPIXEL (default), SLIC or KMEANS)\n"
-         << "  -g <grid>         :: set grid size for superpixel/SLIC segmentation (default: 10)\n"
-         << "  -k <clusters>     :: number of clusters for k-means segmentation (default: 100)\n"
+         << "  (-g <grid>)*      :: set grid size for superpixel/SLIC segmentation (default: 10)\n"
+         << "  (-k <clusters>)*  :: number of clusters for k-means segmentation (default: 100)\n"
          << "  -o <filename>     :: save segmentation to file <filename> (use .txt extension)\n"
          << "                    :: for text; else drwnSuperpixelContainer format is used)\n"
          << "  -a <filename>     :: append segmentation to file <filename> (only supports\n"
@@ -52,8 +52,8 @@ void usage()
 int main(int argc, char *argv[])
 {
     const char *method = "SUPERPIXEL";
-    unsigned gridSize = 10;
-    unsigned numClusters = 100;
+    vector<unsigned> gridSize;
+    vector<unsigned> numClusters;
     const char *outFile = NULL;
     const char *appendFile = NULL;
     bool bMedianBlur = false;
@@ -62,8 +62,12 @@ int main(int argc, char *argv[])
     // process commandline arguments
     DRWN_BEGIN_CMDLINE_PROCESSING(argc, argv)
         DRWN_CMDLINE_STR_OPTION("-m", method)
-        DRWN_CMDLINE_INT_OPTION("-g", gridSize)
-        DRWN_CMDLINE_INT_OPTION("-k", numClusters)
+        DRWN_CMDLINE_OPTION_BEGIN("-g", p)
+            gridSize.push_back(atoi(p[0]));
+        DRWN_CMDLINE_OPTION_END(1)
+        DRWN_CMDLINE_OPTION_BEGIN("-k", p)
+            numClusters.push_back(atoi(p[0]));
+        DRWN_CMDLINE_OPTION_END(1)
         DRWN_CMDLINE_STR_OPTION("-o", outFile)
         DRWN_CMDLINE_STR_OPTION("-a", appendFile)
         DRWN_CMDLINE_BOOL_OPTION("-medianBlur", bMedianBlur)
@@ -77,6 +81,9 @@ int main(int argc, char *argv[])
         return -1;
     }
 
+    if (gridSize.empty()) gridSize.push_back(10);
+    if (numClusters.empty()) numClusters.push_back(100);
+
     drwnCodeProfiler::tic(drwnCodeProfiler::getHandle("main"));
 
     // load image
@@ -89,40 +96,45 @@ int main(int argc, char *argv[])
     }
 
     // run superpixel algorithm
-    cv::Mat seg;
-    if (!strcasecmp(method, "SUPERPIXEL")) {
-        seg = drwnFastSuperpixels(img, gridSize);
-    } else if (!strcasecmp(method, "SLIC")) {
-        cv::Mat imgLab;
-        cv::cvtColor(img, imgLab, CV_BGR2Lab);
-        seg = drwnSLICSuperpixels(imgLab, gridSize * gridSize);
-    } else if (!strcasecmp(method, "KMEANS")) {
-        seg = drwnKMeansSegments(img, numClusters);
-    } else {
-        DRWN_LOG_FATAL("unrecognized method " << method);
-    }
-    DRWN_LOG_VERBOSE(cv::norm(seg, cv::NORM_INF) << " superpixels generated");
-
-    // visualize
-    if (bVisualize) {
-        cv::Mat canvas = img.clone();
-        drwnAverageRegions(canvas, seg);
-        drwnDrawRegionBoundaries(canvas, seg, CV_RGB(0, 0, 0));
-        //cv::Mat canvas = drwnCreateHeatMap(seg);
-        drwnShowDebuggingImage(canvas, string("segmentation"), true);
-    }
-
-    // save segmentation
+    drwnSuperpixelContainer container;
     if (appendFile != NULL) {
-        drwnSuperpixelContainer container;
-
         ifstream ifs(appendFile, ios::binary);
         DRWN_ASSERT_MSG(!ifs.fail(), "file " << appendFile << " does not exist");
         container.read(ifs);
         ifs.close();
+    }
 
-        container.addSuperpixels(seg);
+    if (!strcasecmp(method, "SUPERPIXEL")) {
+        for (unsigned i = 0; i < gridSize.size(); i++) {
+            container.addSuperpixels(drwnFastSuperpixels(img, gridSize[i]));
+        }
+    } else if (!strcasecmp(method, "SLIC")) {
+        cv::Mat imgLab;
+        cv::cvtColor(img, imgLab, CV_BGR2Lab);
+        for (unsigned i = 0; i < gridSize.size(); i++) {
+            container.addSuperpixels(drwnSLICSuperpixels(imgLab, gridSize[i] * gridSize[i]));
+        }
+    } else if (!strcasecmp(method, "KMEANS")) {
+        for (unsigned i = 0; i < numClusters.size(); i++) {
+            container.addSuperpixels(drwnKMeansSegments(img, numClusters[i]));
+        }
+    } else {
+        DRWN_LOG_FATAL("unrecognized method " << method);
+    }
+    DRWN_LOG_VERBOSE(container.size() << " superpixels generated");
 
+    // visualize
+    if (bVisualize) {
+        vector<cv::Scalar> colours(container.size(), cv::Scalar::all(0));
+        for (int segId = 0; segId < container.size(); segId++) {
+            cv::Mat m = container.mask(segId);
+            colours[segId] = cv::mean(img, m);
+        }
+        drwnShowDebuggingImage(container.visualize(img, colours), string("segmentation"), true);
+    }
+
+    // save segmentation
+    if (appendFile != NULL) {
         ofstream ofs (appendFile, ios::binary);
         container.write(ofs);
         ofs.close();
@@ -133,18 +145,18 @@ int main(int argc, char *argv[])
         if (ext.compare("txt") == 0) {
             // write as human readable text file
             ofstream ofs(outFile);
-            for (int y = 0; y < seg.rows; y++) {
-                for (int x = 0; x < seg.cols; x++) {
-                    if (x != 0) ofs << " ";
-                    ofs << seg.at<int>(y, x);
+            for (int i = 0; i < container.channels(); i++) {
+                for (int y = 0; y < container.height(); y++) {
+                    for (int x = 0; x < container.width(); x++) {
+                        if (x != 0) ofs << " ";
+                        ofs << container[i].at<int>(y, x);
+                    }
+                    ofs << "\n";
                 }
-                ofs << "\n";
             }
             ofs.close();
         } else {
             // write as drwnSuperpixelContainer object
-            drwnSuperpixelContainer container;
-            container.addSuperpixels(seg);
             ofstream ofs (outFile, ios::binary);
             container.write(ofs);
             ofs.close();
