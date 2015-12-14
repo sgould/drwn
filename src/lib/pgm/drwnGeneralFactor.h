3a7ab98e37d8da634794ecff2127be4043cde108
Your Name <you@example.com>
2015-12-14 15:58:44 +1100

    Header file for drwnGeneralFactor - either a table factor, sparse factor or sum of two general factors.

---
+++ src/lib/pgm/drwnGeneralFactor.h
@@ -0,0 +1,77 @@
+/*****************************************************************************
+** DARWIN: A FRAMEWORK FOR MACHINE LEARNING RESEARCH AND DEVELOPMENT
+** Distributed under the terms of the BSD license (see the LICENSE file)
+** Copyright (c) 2007-2015, Stephen Gould
+** All rights reserved.
+**
+******************************************************************************
+** FILENAME:    drwnGeneralFactor.h
+** AUTHOR(S):   Stephen Gould <stephen.gould@anu.edu.au>
+**              Albert Chen <chenay@student.unimelb.edu.au>
+**
+*****************************************************************************/
+
+#pragma once
+
+#include "drwnTableFactor.h"
+#include "drwnSparseFactor.h"
+
+enum StorageType 
+{
+	DENSE, SPARSE, EVAL
+};
+
+//! Either a dense factor, sparse factor, or the sum of two drwnGeneralFactors.
+class drwnGeneralFactor : public drwnFactor
+{
+  private:
+	StorageType _storageType;	//!< whether factor is a dense/sparse/evaluation factor
+    drwnTableFactor* _tblFac;		//!< not NULL if this is factor is dense
+    drwnSparseFactor* _sparseFac;	//!< not NULL if this factor is sparse
+    const double THRESHOLD;			//!< if table factor passed in is denser
+						//!< than THRESHOLD, then convert it to a sparse factor
+	vector<int> _variables;         //!< list of variables in factor (by index in factor)
+	const drwnGeneralFactor* op1, *op2;	//!< not NULL if this factor is evaluation
+
+  public:
+    //! create an empty factor
+	drwnGeneralFactor(const drwnVarUniversePtr& ptr) : drwnFactor(ptr),
+			THRESHOLD(0.2) { }
+    ~drwnGeneralFactor() { }
+
+	// access functions
+    const char *type() const { return "drwnGeneralFactor"; }
+    drwnGeneralFactor* clone(void) const { return NULL; }
+
+    //! add variable by id
+	void addVariable(int var);
+	//! return what type of factor this is
+	const StorageType getStorageType() const { return _storageType; }
+	//! make factor represent sum of two general factors
+	void setOps(const drwnGeneralFactor *f1, const drwnGeneralFactor *f2);
+
+    //! Returns the value of the factor for a given (full) assignment
+	double getValueOf(const drwnFullAssignment& y) const;
+    //! Returns the value of the factor for a given partial assignment.
+    double getValueOf(const drwnPartialAssignment& y) const;
+    //! Sets the value of the factor for a given (full) assignment
+    void setValueOf(const drwnFullAssignment& y, double val);
+    //! Sets the value of the factor for a given partial assignment.
+    void setValueOf(const drwnPartialAssignment& y, double val);
+    drwnTableFactor* getTableFactor() const {
+		assert(_tblFac);
+		return _tblFac;
+	}
+    drwnSparseFactor* getSparseFactor() const {
+		assert(_sparseFac);
+		return _sparseFac;
+	}
+	const drwnGeneralFactor* getOp1() const { return op1; }
+	const drwnGeneralFactor* getOp2() const { return op2; }
+
+  private:
+	//! helper function to create sparse factor
+    void subtractMostCommonVal();
+	//! converts table factor to a sparse factor if density is below THRESHOLD
+    void convertIfSparse();
+};
