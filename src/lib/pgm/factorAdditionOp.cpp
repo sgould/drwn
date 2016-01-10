6cffe38cd21c33bdf0fa152925169021e375a58c
Your Name <you@example.com>
2016-01-10 17:51:14 +1100

    Will later commit factorAdditionOp.h which declares the _intendedStorageType member variable. The new idea is to have the constructor determine what the storage type of the target (sum) will be. The class will also *not* declare variables for factors, instead inheriting from drwnFactorNAryOp, which is the whole point.
    The most immediate TODO now seems to be to implement clone for drwnGeneralFactor.
    This should facilitate computing the sum of a vector of drwnGeneralFactors.

--- src/lib/pgm/factorAdditionOp.cpp
+++ src/lib/pgm/factorAdditionOp.cpp
@@ -27,11 +27,14 @@ FactorAdditionOp::FactorAdditionOp(drwnGeneralFactor *target, const drwnGeneralF
 
 	if ((A->getStorageType() == EVAL) || (B->getStorageType() == EVAL) ||
 		(varsA != varsB)) {
+		_intendedStorageType = EVAL;
 		return;
 	}
 
+	/* now have that varsA == varsB */
 	if ((A->getStorageType() == SPARSE) && (B->getStorageType() == SPARSE)) {
 		set<int> targetVars(ordTargetVars.begin(), ordTargetVars.end());	
+		_intendedStorageType = SPARSE;
 
 		for (set<int>::iterator si = targetVars.begin(); si != targetVars.end(); si++) {
 			vector<int>::iterator it = find(ordVarsA.begin(), ordVarsA.end(), *si);
@@ -42,52 +45,42 @@ FactorAdditionOp::FactorAdditionOp(drwnGeneralFactor *target, const drwnGeneralF
 
 		return;
 	}
-}
-<<<<<<< HEAD
 
-void FactorAdditionOp::execute()
-{
-	if (_target->getStorageType() == EVAL) {
-		return;
-	} else if (_target->getStorageType() == SPARSE) {
-		vector<int> vars = _target->getOrderedVars();
-		drwnSparseFactor *fac1 = _A->getSparseFactor();
-		drwnSparseFactor *fac2 = _B->getSparseFactor();
-		drwnSparseFactor *target = _target->getSparseFactor();
-		map<vector<int>, double> assignmtsA = fac1->getAssignments();
-		map<vector<int>, double> assignmtsB = fac2->getAssignments();
-
-		for (map< vector<int>, double >::iterator mi = assignmtsA.begin();
-				mi != assignmtsA.end(); mi++) {
-			vector<int> vals(vars.size());
-
-			for (int i = 0; i < vars.size(); i++) {
-				vals[_targetToA[i]] = (*mi).first[_targetToA[i]];
-			}
-
-			drwnPartialAssignment dpa;
-			target->setValueOf(dpa, (*mi).second);
-		}
-
-		for (map< vector<int>, double >::iterator mi = assignmtsB.begin();
-				mi != assignmtsA.end(); mi++) {
-			vector<int> vals(vars.size());
-
-			for (int i = 0; i < vars.size(); i++) {
-				vals[_targetToB[i]] = (*mi).first[_targetToB[i]];
-			}
-
-			drwnPartialAssignment dpa;
-			target->setValueOf(dpa, target->getValueOf(dpa) + (*mi).second);
-		}
-
+	if ((A->getStorageType() == DENSE) && (B->getStorageType() == DENSE)) {
+		_intendedStorageType = DENSE;
 		return;
 	}
 
-	/* dealing with dense factors */
-	drwnTableFactor *target = _target->getTableFactor();
-	drwnFactorAdditionOp dfao(target, _A->getTableFactor(), _B->getTableFactor());
-	dfao.execute();
+	/* have that varsA == varsB and adding dense to sparse */
+	_intendedStorageType = EVAL;	/* temporary hack */
+	/* _intendedStorageType = DENSE; */
+	/* TODO: convert sparse factor to dense factor */
+}
+<<<<<<< HEAD
+
+FactorAdditionOp::FactorAdditionOp(drwnGeneralFactor *target,
+		const vector<const drwnGeneralFactor *>& A) :
+    drwnFactorNAryOp(target, A)
+{	// do nothing
+}
+
+void FactorAdditionOp::execute()
+{
+	if (_generalFactors.size() == 2) {
+		if (_intendedStorageType == EVAL) {
+			return;
+		} else if (_intendedStorageType == SPARSE) {
+			return;
+		}
+
+		/* dealing with dense factors */
+		drwnTableFactor *target = _generalTarget->getTableFactor();
+		drwnFactorAdditionOp dfao(target, _generalFactors[0]->getTableFactor(),
+				_generalFactors[1]->getTableFactor());
+		dfao.execute();
+		_generalTarget = new drwnGeneralFactor(*target, _generalTarget->THRESHOLD);
+		return;
+	}
 }
 =======
 >>>>>>> 5477d5d4f80732bdb8e6f07f513126e864b9fc40
